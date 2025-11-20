5902b04f609c6d56ebbab2f1a97413aa18c57da6
Evelin Moreno <evelinmaria123moreno@gmail.com>
2025-11-20 08:28:15 -0500

    Avances de la clase 17 y 18 del mes 09

--- primera-app/src/cashier/cashier.controller.ts
+++ primera-app/src/cashier/cashier.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+} from '@nestjs/common';
 
 @Controller('cashier')
-export class CashierController {}
+export class CashierController {
+    @Post()
+    createSalesRecord() {
+        return 'Tu registro de ventas ha sido creado correctamente.';
+    }
+}
